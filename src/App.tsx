2437b987dfac0557dd3bdaa3d41d94841974ae37
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-04-24 17:27:11 -0300

    chore: add eslint and prettier initial configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/ui/button";
+import { Loader2 } from "lucide-react";
+
 export function App() {
-  return <h1>Hello, world</h1>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button variant={"default"}>
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </Button>
+    </div>
+  );
 }
