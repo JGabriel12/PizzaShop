74266b4d031c018e5c71ca50081d676c72643f51
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-04-24 17:40:38 -0300

    chore: add react-router-dom initial configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { Button } from "./components/ui/button";
-import { Loader2 } from "lucide-react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button variant={"default"}>
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        Please wait
-      </Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
