d6b50c0f749e884352d7e9e5e6313fc65d043293
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-05-08 18:03:53 -0300

    feat: page 404 not found error

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import Dashboard from "./pages/app/Dashboard";
+import Dashboard from "./pages/app/dashboard/Dashboard";
 import SignIn from "./pages/auth/Sign-in";
 import SignUp from "./pages/auth/Sign-up";
 
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
